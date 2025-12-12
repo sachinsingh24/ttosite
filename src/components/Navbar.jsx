8fe9ef2927d69800075a1af5d910591e17db8d9e
sachin singh <ssingh11@opentext.com>
2025-12-12 12:53:59 +0530

    Enhance Navbar: Add closeMenu function to collapse menu on link click

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,21 +3,60 @@ import { Link, NavLink } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Navbar() {
+  const closeMenu = () => {
+    const navbar = document.getElementById("navbarNav");
+    const bsCollapse = window.bootstrap.Collapse.getInstance(navbar);
+    if (bsCollapse) bsCollapse.hide();
+  };
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-white shadow-sm py-3 fixed-top">
 
       <div className="container">
         <Link className="navbar-brand fw-bold" to="/">MyLogo</Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
+
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li className="nav-item"><NavLink className="nav-link" to="/" end>Home</NavLink></li>
-            <li className="nav-item"><NavLink className="nav-link" to="/about">About</NavLink></li>
-            <li className="nav-item"><NavLink className="nav-link" to="/services">Services</NavLink></li>
-            <li className="nav-item"><NavLink className="nav-link" to="/team">Team</NavLink></li>
-            <li className="nav-item"><NavLink className="nav-link" to="/contact">Contact</NavLink></li>
+
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/" end onClick={closeMenu}>
+                Home
+              </NavLink>
+            </li>
+
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/about" onClick={closeMenu}>
+                About
+              </NavLink>
+            </li>
+
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/services" onClick={closeMenu}>
+                Services
+              </NavLink>
+            </li>
+
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/team" onClick={closeMenu}>
+                Team
+              </NavLink>
+            </li>
+
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/contact" onClick={closeMenu}>
+                Contact
+              </NavLink>
+            </li>
+
           </ul>
         </div>
       </div>
